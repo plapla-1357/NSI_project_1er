c33314d50b82def005172f1096285920c8914d62
Gaëtan <73687455+plapla-1357@users.noreply.github.com>
2022-04-16 22:44:04 +0200

    first commit

---
+++ src/js/script.js
@@ -0,0 +1,49 @@
+const nav = document.getElementsByTagName("nav")[0]
+const li = nav.getElementsByTagName("li")
+
+let path
+let reload_nav = ()=>{  //selectione le bon boutton dans la nav
+    path = window.location.pathname
+    for (let i = 0; i < li.length; i++) {
+        li[i].classList.remove("selected")
+    }
+    if (path =="/" | path =="/index.html") {
+        li[0].classList.add("selected")
+    }else if (path == "/presentation.html"){
+        li[1].classList.add("selected")
+    }else if (path == "/hist_info.html"){
+        li[2].classList.add("selected")
+    }else if (path == "/sources.html"){
+        li[3].classList.add("selected")
+    }
+}
+
+
+reload_nav()
+
+for (let j = 0; j < 4; j++) {
+    li[j].addEventListener("click", () => {
+        setTimeout(() => {reload_nav()}, 2)
+    })
+}
+
+const menu_button = document.getElementById("menu_button")
+const menu = document.getElementById("menu")
+
+menu_button.addEventListener("click", () => {
+    let main = document.getElementById("swup")
+    menu_button.classList.toggle("open")
+    menu.classList.toggle("open")
+    main.classList.toggle("closed")
+
+})
+
+function easter(){
+    console.log("click")
+    alert("you found the easter egg!! 😊")
+}
+
+
+
+
+
